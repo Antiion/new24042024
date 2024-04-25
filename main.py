693bd2f39a1442fd70cdb8d6056fcb5dd0789e2b
Gotgilf A <gotgilf92@gmail.com>
2024-04-25 17:57:34 +0300

    добавление .py файла

---
+++ main.py
@@ -0,0 +1,14 @@
+# # import task_list
+import math
+
+# total = 0
+# fac = 1
+#
+# for i in range(1, 5):
+#     fac *= i
+#     total += fac
+#     # print(f'i = {i}, fac = {fac}, summ = {total}')
+#
+# print(total)
+print(math.sin(math.degrees(60)))
+
